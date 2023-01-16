8500b35154a5286883b4216ea02c95a9121f7de9
Hermann <github@doetsch-hermann.de>
2023-01-16 14:12:13 +0100

    initial

--- VictronModbus/module.php
+++ VictronModbus/module.php
@@ -1,12 +1,11 @@
 <?php
-class vmb extends IPSModule {
+class VMB extends IPSModule {
     public function __construct($InstanceID) {
 
         //Never delete this line!
         parent::__construct($InstanceID);
 
     }
-
     public function Create() {
 
         //Never delete this line!
@@ -17,7 +16,7 @@ class vmb extends IPSModule {
         $this->RegisterPropertyInteger("Poller", 0);
         $this->RegisterPropertyInteger("Phase", 1);
 
-        $this->RegisterTimer("Poller", 0, "vmb_RequestRead(\$_IPS['TARGET']);");
+        $this->RegisterTimer("Poller", 0, "VMB_RequestRead(\$_IPS['TARGET']);");
 
     }
 
