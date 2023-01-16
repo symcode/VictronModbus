659bc2808bf5911f0d99122363edf0f60c018162
Hermann <github@doetsch-hermann.de>
2023-01-17 00:06:35 +0100

    initial

--- VictronModbus/VictronModbusRegister.php
+++ VictronModbus/VictronModbusRegister.php
@@ -13,7 +13,6 @@
  */
 class VictronModbusRegister
 {
-
     const value_addresses = [
 
             /**
@@ -21,14 +20,12 @@ class VictronModbusRegister
              */
             800 => [
                 'name' => 'Serial (System)',
-                'count' => 6,
-                'type' => 'U16',
+                'count' => 6,                
                 'scale' => 0
             ],
             806 => [
                 'name' => 'CCGX Relay 1 state',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'mapping' => [
                     0 => 'open',
@@ -37,8 +34,7 @@ class VictronModbusRegister
             ],
             807 => [
                 'name' => 'CCGX Relay 2 state',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'mapping' => [
                     0 => 'open',
@@ -47,134 +43,115 @@ class VictronModbusRegister
             ],
             808 => [
                 'name' => 'PV - AC-coupled on output L1',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             809 => [
                 'name' => 'PV - AC-coupled on output L2',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             810 => [
                 'name' => 'PV - AC-coupled on output L3',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             811 => [
                 'name' => 'PV - AC-coupled on input L1',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             812 => [
                 'name' => 'PV - AC-coupled on input L2',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             813 => [
                 'name' => 'PV - AC-coupled on input L3',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             814 => [
                 'name' => 'PV - AC-coupled on generator L1',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             815 => [
                 'name' => 'PV - AC-coupled on generator L2',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             816 => [
                 'name' => 'PV - AC-coupled on generator L3',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             817 => [
                 'name' => 'AC Consumption L1',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             818 => [
                 'name' => 'AC Consumption L2',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             819 => [
                 'name' => 'AC Consumption L3',
-                'count' => 1,
-                'type' => 'U16',
+                'count' => 1,                
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             820 => [
                 'name' => 'Grid L1',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             821 => [
                 'name' => 'Grid L2',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             822 => [
                 'name' => 'Grid L3',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             823 => [
                 'name' => 'Genset L1',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             824 => [
                 'name' => 'Genset L2',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             825 => [
                 'name' => 'Genset L3',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             826 => [
                 'name' => 'Active input source',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'mapping' => [
                     0 => 'Unknown',
@@ -186,35 +163,30 @@ class VictronModbusRegister
             840 => [
                 'name' => 'Battery Voltage (System)',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 10,
                 'profile' => '~Volt'
             ],
             841 => [
                 'name' => 'Battery Current (System)',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 10,
                 'profile' => '~Ampere'
             ],
             842 => [
                 'name' => 'Battery Power (System)',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             843 => [
                 'name' => 'Battery State of Charge (System)',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 1,
                 'profile' => '~Battery.100'
             ],
             844 => [
                 'name' => 'Battery state (System)',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 1,
                 'mapping' => [
                     0 => 'idle',
@@ -225,42 +197,36 @@ class VictronModbusRegister
             845 => [
                 'name' => 'Battery Consumed Amphours (System)',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 10,
                 'profile' => '~Electricity'
             ],
             846 => [
                 'name' => 'Battery Time to Go (System)',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 100,
                 'profile' => 'Hours'
             ],
             850 => [
                 'name' => 'PV - DC-coupled power',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             851 => [
                 'name' => 'PV - DC-coupled current',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 10,
                 'profile' => '~Ampere'
             ],
             855 => [
                 'name' => 'Charger power',
                 'count' => 1,
-                'type' => 'U16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ],
             860 => [
                 'name' => 'DC System Power',
                 'count' => 1,
-                'type' => 'S16',
                 'scale' => 1,
                 'profile' => 'Watt'
             ]
